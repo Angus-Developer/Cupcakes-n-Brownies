5a6f9755e6aa36d2dc17ead66eb4a99aaeff7179
Angus-Developer <angusbrowning.switch@gmail.com>
2024-11-18 14:44:59 +1100

    Buttons maybe?

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('myButton').addEventListener('click', () => {
     alert('Button clicked!');
 });
+
+document.getElementById('icup1').addEventListener('click', () => {
+    window.location.href = "cupcake.html";
+});
